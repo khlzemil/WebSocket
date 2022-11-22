96e33667320bd3287dbe5291c1e3fe34b353ee32
khlzemil <emilmg@code.edu.az>
2022-11-22 04:05:13 +0400

    WebSocket

---
+++ Chat/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Chat.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Chat.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public HomeController()
+        {
+            
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+       
+    }
+}
